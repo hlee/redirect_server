bdbfe0e4d91b8f9c9d24f0be8a07eb4031e0f54e
Ken Lu <ken@seeice.com>
2012-09-01 21:18:55 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedirectServer::Application.config.secret_token = 'f7751d8a2e05fe8896cd2f9213a49122fc99f0bb8fd6e2b106ba9455e13d7de359148855b3de1b2724f9bdbac1526153b9838bae235845cd69667b8f2fcbe763'
